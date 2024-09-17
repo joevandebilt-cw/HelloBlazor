4d54b0b59658ba079924fce71c9f563f0168fc33
Joe van de Bilt <joe.van.de.bilt@codewizards.co.uk>
2024-09-17 14:33:19 +0100

    Maui Module 5-5

---
+++ CoffeeBrowser.BlazorWeb/Data/CoffeeService.cs
@@ -0,0 +1,20 @@
+﻿using CoffeeBrowser.Library.Data;
+
+namespace CoffeeBrowser.BlazorWeb.Data;
+
+public class CoffeeService : ICoffeeService
+{
+    private readonly IHttpClientFactory _factory;
+
+    public CoffeeService(IHttpClientFactory factory)
+    {
+        _factory = factory;
+    }
+    public async Task<IEnumerable<Coffee>?> LoadCoffeeAsync()
+    {
+        using var _httpClient = _factory.CreateClient();
+
+        var coffees = await _httpClient.GetFromJsonAsync<IEnumerable<Coffee>>("https://thomasclaudiushuber.com/pluralsight/coffees.json");
+        return coffees;
+    }
+}
